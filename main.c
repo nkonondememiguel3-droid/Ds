cec621f9e0ff9d06b2b3635bf03fce4285199afc
Nkono Ndeme Miguel <nkonondememiguel7@gmail.com>
2026-07-12 17:51:57 +0100

    updated the arena to use custom allocators

--- main.c
+++ main.c
@@ -1,11 +1,30 @@
+#include <mkl_service.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "ds_arena.h"
 #include "ds_dyn_array.h"
 
+void *alloc_mkl(size_t bytes, void *user_data) {
+  (void)user_data;
+  return mkl_malloc(bytes, 64);
+}
+
+void *realloc_mkl(void *ptr, size_t new_size, void *user_data) {
+  (void)user_data;
+  return mkl_realloc(ptr, new_size);
+}
+
+void free_mkl(void *ptr, void *user_data) {
+  (void)user_data;
+  MKL_free(ptr);
+}
+
 int main(void) {
-  _ds_arena_t_ arena = ds_arena_new(0);
+  /* _ds_arena_t_ arena = ds_arena_new(0); */
+  _ds_arena_t_ arena = ds_arena_new_with_allocator(0, alloc_mkl, realloc_mkl, free_mkl, NULL);
+
+  /* int *numbers = ARENA_NEW(&arena, int); */
 
   int *numbers = NULL;
 
